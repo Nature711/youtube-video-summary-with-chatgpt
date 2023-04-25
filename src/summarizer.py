d1953b9a5083eb38bbdb0e3fb87535a1f4ac6edc
Nature711 <e0665488@u.nus.edu>
2023-04-25 23:07:03 +0800

    fix bug in summarizer

--- src/summarizer.py
+++ src/summarizer.py
@@ -7,15 +7,30 @@ load_dotenv()
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
+def split_string_into_chunks(text, K):
+    words = text.split()
+    chunks = []
+    current_chunk = ""
+    for word in words:
+        if len(current_chunk.split()) + len(word.split()) <= K:
+            current_chunk += " " + word
+        else:
+            chunks.append(current_chunk.strip())
+            current_chunk = word
+    if len(current_chunk.split()) > 0:
+        chunks.append(current_chunk.strip())
+    return chunks
+
 def generate_summary(transcript):
+
+    if (transcript is None): return "No summary available"
+
     # Summarize the transcript using OpenAI's GPT-3 API
-    max_token_limit = 2000
-    chunk_size = max_token_limit - 200
-    transcript_chunks = [transcript[i:i+chunk_size] for i in range(0, len(transcript), chunk_size)]
+    max_chunk_size = 3500
+    transcript_chunks = split_string_into_chunks(transcript, max_chunk_size)
     summary = ''
-    prev_summary = ''
-    for i, chunk in enumerate(transcript_chunks):
-        text = prev_summary + chunk
+    for i in range(len(transcript_chunks)): 
+        text = summary + transcript_chunks[i]
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=f"Please summarize the following text:\n{text}",
@@ -26,7 +41,17 @@ def generate_summary(transcript):
             timeout=60,
         )
         summary += response.choices[0].text
-        prev_summary = summary
 
-    return summary
+    final_summary = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Please summarize the following text:\n{summary}",
+            temperature=0.5,
+            max_tokens=200,
+            n = 1,
+            stop=None,
+            timeout=60,
+        ).choices[0].text
+
+    return final_summary
+    
 
