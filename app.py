91787deaa5e53760817e664661ca8c9740ce3490
Nature711 <e0665488@u.nus.edu>
2023-04-25 23:06:02 +0800

    add global variable for last fetch time

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+from datetime import datetime, timedelta
 from flask import Flask, render_template
 from src.report_generator import generate_reports
 
 app = Flask(__name__)
 
+# Set the initial time to one hour ago
+last_fetch_time = datetime.now() - timedelta(hours=1)
+
 @app.route('/')
 def index():
+    global last_fetch_time
+
     # Generate reports
-    reports = generate_reports()
+    reports = generate_reports(last_fetch_time)
     return render_template('index.html', reports=reports)
 
 if __name__ == '__main__':
